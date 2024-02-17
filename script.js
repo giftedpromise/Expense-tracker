515fed95470a67620d922aa9325107d446f0e4a0
giftedpromise <giftedpromise1992@gmail.com>
2024-02-17 23:52:50 +0100

    expense tracker

---
+++ script.js
@@ -0,0 +1,7 @@
+const inputFieldEl = document.getElementById("inputTitle");
+const addTransactionBtnEl = document.getElementById("transaction-btn");
+const historyList = document.getElementById("list");
+
+addTransactionBtnEl.addEventListener("click", function () {
+  let transactionInput = inputFieldEl.value;
+});
