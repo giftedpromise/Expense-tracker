fb3d5d20cfea41858c703e90a32688c0c4f33907
giftedpromise <giftedpromise1992@gmail.com>
2024-02-18 02:59:11 +0100

    set up data base

--- script.js
+++ script.js
@@ -15,16 +15,39 @@ const transactionInDB = ref(database, "transaction");
 const inputFieldEl = document.getElementById("inputTitle");
 const inputAmountEl = document.getElementById("inputAmount");
 const addTransactionBtnEl = document.getElementById("transaction-btn");
+const amountTypeEl = document.getElementById("amountType");
+const listEl = document.getElementById("list");
 
-addTransactionBtnEl.addEventListener("click", function () {
+addTransactionBtnEl.addEventListener("click", function (event) {
+  if (
+    !inputFieldEl ||
+    !inputAmountEl ||
+    !addTransactionBtnEl ||
+    !amountTypeEl
+  ) {
+    console.error("One or more input elements not found.");
+    return;
+  }
+
+  event.preventDefault();
+
   let transactionInput = inputFieldEl.value;
+  let amountInput = inputAmountEl.value;
+  let amountType = amountTypeEl.value;
+  const date = new Date();
+  let day = date.getDate();
+  let month = date.getMonth() + 1;
+  let year = date.getFullYear();
+  const currentDate = `${day}-${month}-${year}`;
+  console.log(currentDate);
 
   push(transactionInDB, {
+    id: Date.now(),
+    date: currentDate,
     description: transactionInput,
-    amount: 0, // You may need to add an amount field to your input form
-    type: "Expense", // Set default type to Expense, modify based on your UI
-    date: new Date().toString(), // Set the date to the current date
+    amount: amountInput,
+    amountType, // You may need to add an amount field to your input form
   });
 
-  console.log(transactionInput);
+  inputFieldEl.value = "";
 });
