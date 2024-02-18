f4752e27cc8aeff3420dd6ba0a58b2c7c40b4c82
giftedpromise <giftedpromise1992@gmail.com>
2024-02-18 02:24:06 +0100

    set up data base

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  push,
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+const appSettings = {
+  databaseURL: "https://expense-tracker-9c5fb-default-rtdb.firebaseio.com/",
+};
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const transactionInDB = ref(database, "transaction");
+
 const inputFieldEl = document.getElementById("inputTitle");
+const inputAmountEl = document.getElementById("inputAmount");
 const addTransactionBtnEl = document.getElementById("transaction-btn");
-const historyList = document.getElementById("list");
 
 addTransactionBtnEl.addEventListener("click", function () {
   let transactionInput = inputFieldEl.value;
+
+  push(transactionInDB, {
+    description: transactionInput,
+    amount: 0, // You may need to add an amount field to your input form
+    type: "Expense", // Set default type to Expense, modify based on your UI
+    date: new Date().toString(), // Set the date to the current date
+  });
+
+  console.log(transactionInput);
 });
